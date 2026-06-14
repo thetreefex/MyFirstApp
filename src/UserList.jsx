503730948661e8ee166db7ed2bd1d8730aa070c2
Treefex <thetreefex@gmail.com>
2026-06-14 19:57:11 +0200

    react Commit

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -1,12 +1,15 @@
-function checkServer() {
-  const data = null
-  const req = async() => {
-    const res = await fetch("http://localhost:3000/")
-    data = await res.json
-  }
-  req()
+import { useState,useEffect } from "react"
+function CheckServer() {
+  const [message,setMessage] = useState("Loading")
+  useEffect(() => {
+    const req = async() => {
+      const res = await fetch("http://localhost:3000/")
+      const data = await res.json()
+      setMessage(data.message)}
+    req()
+  },[])
   return(
-    <h2>{data}</h2>
+    <h2>{message}</h2>
   )
 }
-export default checkServer
+export default CheckServer
