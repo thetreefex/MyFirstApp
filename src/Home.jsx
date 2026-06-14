ff9b2a0f021e502f9c6fb81453493af68e7eef25
Treefex <thetreefex@gmail.com>
2026-06-14 20:03:34 +0200

    react Commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,8 @@
 import { Routes,Route, Link } from "react-router-dom"
 import "./All.css"
-import UserList from "./UserList"
 import About from "./About"
 import Users from "./Users"
-import CheckServer from "./UserList"
+import CheckServer from "./CheckServer"
 function Home() {
     return (
         <div>
@@ -18,13 +17,13 @@ function App() {
                 <Link id="Link" to={"/"}>Main</Link>
                 <Link id="Link" to={"/About"}>About</Link>
                 <Link id="Link" to={"/Users"}>Users</Link>
-                <Link id="Link" to={"/UserList"}>Search User</Link>
+                <Link id="Link" to={"/CheckServer"}>Search User</Link>
             </nav>
             <Routes>
                 <Route path="/" element={<Home/>} />
                 <Route path="/About" element={<About/>}/>
                 <Route path="/Users" element={<Users/>}/>
-                <Route path="UserList" element={<CheckServer/>}/>
+                <Route path="CheckServer" element={<CheckServer/>}/>
             </Routes>
         </div>
     )
